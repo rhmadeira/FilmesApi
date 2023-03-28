30cc9827dd254ae05ba782301556c5df9c49cb39
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-03-28 00:38:09 -0300

    feat: create request

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -8,12 +8,28 @@ namespace FilmesApi.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 1;
 
     [HttpPost]
-    public void AdcionaFilme([FromBody] Filme filme)
+    public void AddFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Duracao);
         Console.WriteLine(filme.Titulo);
+        
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> GetAllFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? GetFilmeById(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
+        
     }
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,10 +1,20 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+
+
+    [Required(ErrorMessage = "O titulo do filme é obrigatório")]
     public string Titulo { get; set; }
 
+    [Required(ErrorMessage = "O genero do filme é Obrigatório")]
+    [MaxLength(50, ErrorMessage = "O genero nã pode exceder 50 caracteres")]
     public string Genero { get; set; }
 
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ter no mínimo 70 e no máximo 600 minutos")]
     public int Duracao { get; set; }
 }
