57b0369b0a3d7da66814be1cb1abcbbb0fe082a3
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-07-30 11:27:11 -0300

    Dto Create filme

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,4 +1,6 @@
-﻿using FilmesApi.Data;
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
 using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,15 +11,19 @@ namespace FilmesApi.Controllers;
 public class FilmeController : ControllerBase
 {
     private FilmeContext _context;
+    private IMapper _mapper;
 
-    public FilmeController(FilmeContext context)
+    public FilmeController(FilmeContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
     [HttpPost]
-    public IActionResult AddFilme([FromBody] Filme filme)
+    public IActionResult AddFilme([FromBody] CreateFilmeDto filmeDto)
     {
+
+        Filme filme = _mapper.Map<Filme>(filmeDto);
         _context.Filmes.Add(filme);
         _context.SaveChanges();
       return  CreatedAtAction(nameof(GetFilmeById), new { id = filme.Id }, filme);
---
+++ FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos;
+
+public class CreateFilmeDto
+{
+
+    [Required(ErrorMessage = "O titulo do filme é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O genero do filme é Obrigatório")]
+    [StringLength(50, ErrorMessage = "O genero nã pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ter no mínimo 70 e no máximo 600 minutos")]
+    public int Duracao { get; set; }
+}
